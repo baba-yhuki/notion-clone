b8d541a20cc3eba29a1ad5eeec96d49265a0ecba
baba-yhuki <yuki66webdesign@gmail.com>
2025-03-25 22:26:30 +0900

    Editor.tsx修正

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,6 @@
 import { locales } from "@blocknote/core";
-import { useCreateBlockNote } from "@blocknote/react";
+import { useCreateBlockNote, BlockNoteView } from "@blocknote/react";
 import "@blocknote/mantine/style.css";
-import { BlockNoteView } from "@blocknote/mantine";
 
 interface EditorProps {
   onChange: (value: string) => void;
